f5f64758c28921a4b06d45eb2047437c84956805
Stacia Johanna <staciaj@mit.edu>
2021-04-23 14:15:21 -0400

    add clear canvas

--- draw.js
+++ draw.js
@@ -42,3 +42,18 @@ Leap.loop(controllerOptions, function(frame, done) {
     draw();
     //done();
 });
+
+/**
+ * reference: https://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
+ */
+function clearCanvas() {
+    // Store the current transformation matrix
+    ctx.save();
+
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Restore the transform
+    ctx.restore();
+}
